1bf3769f7b8cc8b1c35e244705f4d47f3e8d8c74
Milan Dala <codmilan@hotmail.com>
2022-05-30 17:59:00 +0200

    viewport width

--- scripts/main.js
+++ scripts/main.js
@@ -21,21 +21,3 @@ else if( urlLastPart === "comments.html")
 {
     navBarChildren[3].classList.add("active-page")
 }
-/* $(".navbar-right-bottom")
-    .find("a")
-    .map(function() {
-        return { object: this, href: $(this).attr("href") };
-    })
-    .get()
-    .forEach(link => {
-        if (link.href.split("/")[4].includes(url)) {
-            if (!(url === "about" || url === "opening")) {
-                $(link.object).addClass("navbar-bottom-active");
-            } else if (url.includes("about")) {
-                $("#about-dropdown").addClass("navbar-bottom-active");
-            } else {
-                $("#opening-dropdown").addClass("navbar-bottom-active");
-            }
-        }
-    });
-    */
